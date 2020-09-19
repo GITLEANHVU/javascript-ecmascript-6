b745c2bb78951ece2c289fb318d1f129d6ea63a4
Lê Anh Vũ <leanhvu>
2020-09-19 18:52:09 +0700

    upload file function.js

---
+++ function.js
@@ -0,0 +1,13 @@
+// function sumTwoNum(x, y) {
+//   return x + y;
+// }
+// const total = sumTwoNum(2, 1);
+// console.log(total);
+
+// const sumTwoNum = (x, y) => {
+//   return x + y;
+// }
+// const sumTwoNum = (x, y) => x + y
+const sumTwoNum = (x, y) => (x + y)
+
+console.log(sumTwoNum(2, 5))
